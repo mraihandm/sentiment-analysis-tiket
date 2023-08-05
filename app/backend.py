6b1143d44b32a0e2576a04b9567436d7c0833a35
Raihan Dzaky <83358796+mraihandm@users.noreply.github.com>
2023-08-05 18:48:07 +0700

    Add files via upload

---
+++ app/backend.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import numpy as np
+import re
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import string
+import json
+
+from nltk.tokenize import word_tokenize
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemover, ArrayDictionary
+from nltk.probability import FreqDist
+from nltk.util import ngrams
+from matplotlib import pyplot as plt
+from collections import Counter
+from wordcloud import WordCloud
+
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+
+# %%
+def text_filtering(text):
+    text = text.lower()     # mengubah review menjadi lowercase
+    text = re.sub(r'https?:\/\/\S+','',text)        # menghilangkan url
+    text = ' '.join(re.sub("([@#][A-Za-z0-9]+)|(\w+:\/\/\S+)"," ",text).split())        # menghilangkan mention & hashtag
+    text = re.sub(r'(b\'{1,2})',"", text)       # menghilangkan karakter byte
+    text = re.sub('[^a-zA-Z]', ' ', text)       # menghilangkan karakter bukan huruf
+    text = re.sub(r'\d+', '', text)       # menghilangkan digit angka
+    text = text.translate(str.maketrans("","",string.punctuation))      #menghilangkan tanda baca
+    text = re.sub(r'\s+', ' ', text).strip()        # menghilangkan whitespace
+    return text
+
+# %%
+def stopstem(text):
+    with open("kamus_stopwords_v2.txt") as kamus:
+        word = kamus.readlines()
+        list_stopword = [line.replace('\n',"") for line in word]
+    dictionary = ArrayDictionary(list_stopword)
+    stopword = StopWordRemover(dictionary)
+    text = stopword.remove(text)
+    #stem
+    factory_stemmer = StemmerFactory()
+    stemmer = factory_stemmer.create_stemmer()
+    text = stemmer.stem(text)
+    return text 
+
+# %%
+def word_tokenize_wrapper(text):
+    return word_tokenize(text)
+
+# %%
+#pengubahan kata slang
+kamusslang = pd.read_csv("kamus_slangwords.csv")
+kata_pembakuan_dict = {}
+
+for index, row in kamusslang.iterrows():
+    if row[0] not in kata_pembakuan_dict:
+        kata_pembakuan_dict[row[0]] = row[1] 
+
+
+# %%
+def pembakuan_kata(document):
+    return [kata_pembakuan_dict[term] if term in kata_pembakuan_dict else term for term in document]
+
+# %%
+#penggabungan token pembakuan menjadi kalimat
+def join_token(document):
+    for i in range(len(document)):
+        document[i] = ' '.join(document[i])
+    return document
+
+# %%
+#penghapusan stopwords akhir
+def stop(text):
+    with open("kamus_stopwords_v2.txt") as kamus:
+        word = kamus.readlines()
+        list_stopword = [line.replace('\n',"") for line in word]
+    dictionary = ArrayDictionary(list_stopword)
+    stopword = StopWordRemover(dictionary)
+    text = stopword.remove(text)
+    return text
+
+# %%
+def temptoken(document):
+    return document.apply(lambda x: word_tokenize(str(x)))
+
+# %%
+#Perhitungan Frekuensi Kata
+def hitung_kata(df,column='temp_token', preprocess=None, min_freq=2):
+    def update(doc):
+        tokens = doc if preprocess is None else preprocess(doc)
+        counter.update(tokens)
+    counter = Counter()
+    df[column].map(update)
+    freq_df = pd.DataFrame.from_dict(counter, orient= 'index', columns=['freq'])
+    freq_df = freq_df.query('freq >= @min_freq')
+    freq_df.index.name = 'token'
+    return freq_df.sort_values('freq', ascending=False)
+
+# %%
+def wordcloud(doc):
+    text = ' '.join(doc.tolist())
+    wc = WordCloud(width=800, height=400, background_color='white').generate(text)
+
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wc, interpolation='bilinear')
+    plt.title("Wordcloud")
+    plt.axis('off')
+    plt.show()
+
+# %%
+def ngrams(tokens, n=2, sep=' ', stopwords=set()):
+    return [sep.join(ngram) for ngram in zip(*[tokens[i:] for i in range(n)]) if len([t for t in ngram if t in stopwords])==0]
+
+# %%
+def convertdf_tocsv(df):
+    return df.to_csv().encode('utf-8')
+
+# %%
+def vectorizzer(df):
+    tfidf = TfidfVectorizer(min_df= 10, ngram_range=(1,1))
+    x_tf = tfidf.transform(df.apply(lambda x: np.str(x)))
+    return x_tf
+
+
---
+++ app/dashlit.py
@@ -0,0 +1,223 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+from nltk.tokenize import word_tokenize
+import plotly.express as px
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import joblib
+
+from google_play_scraper import Sort, reviews_all
+
+from backend import text_filtering, stopstem, word_tokenize_wrapper
+from backend import pembakuan_kata, stop, hitung_kata, ngrams
+from backend import convertdf_tocsv
+
+
+@st.cache_data
+def scrape_reviews():
+  tiket_review = reviews_all(
+      'com.tiket.gits',
+      sleep_milliseconds=0,
+      lang='id',
+      country='id',
+      sort=Sort.NEWEST
+  )
+  df = pd.DataFrame(tiket_review)
+  df['date'] = pd.to_datetime(df['at'], format = '%Y%m%d') 
+  return df
+
+def date_selector(start_date, end_date,df):
+  df_filter = df.loc[((df['date'] >= start_date) & (df['date'] <= end_date))]
+  return df_filter
+
+if 'stage' not in st.session_state:
+    st.session_state.stage = 0
+
+def set_state(i):
+    st.session_state.stage = i
+
+svm_model = joblib.load('svm_ready.joblib')
+vectorizer = joblib.load('vectorizer_svm_ready.joblib')
+
+
+def main():
+  activities = ["Main Page", "Tentang Kami"]
+  choice = st.sidebar.selectbox("Choice", activities)
+
+  if choice == 'Main Page':
+    st.title("Aplikasi Analisis Sentimen")
+    st.markdown("Aplikasi ini dibuat untuk analisis sentimen")
+
+    st.subheader("Scrap Data Ulasan dari Google Play Store")
+
+    col1, col2 = st.columns (2)
+    with col1:
+      d = st.date_input("Tanggal awal", datetime.date(2023,1,1))
+    with col2:
+      f = st.date_input("Tanggal akhir", datetime.date(2023,2,1))
+    d = d.strftime('%Y-%m-%d')
+    f = f.strftime('%Y-%m-%d')
+
+    if st.button("Mulai scraping"):
+      df = scrape_reviews()
+      df_filter = date_selector(d,f,df)
+      
+      st.markdown('**Data hasil scraping**')
+      st.dataframe(df_filter)
+
+      fig1 = px.histogram(df_filter, x='score', title='Sebaran rating')
+      fig1.update_layout(showlegend=False)
+      fig1.update_layout(bargap=0.1)
+      fig1.update_traces(marker=dict(color='#50C878'))
+      st.plotly_chart(fig1)
+
+      dfprep = df_filter[['content','score']]
+      dfprep['filtered'] = dfprep['content'].apply(text_filtering)
+      dfprep['cleaned'] = dfprep['filtered'].apply(stopstem)
+      dfprep['tokens'] = dfprep['cleaned'].apply(word_tokenize_wrapper)
+      
+      kamusslang = pd.read_csv("kamus_slangwords.csv")
+      kata_pembakuan_dict = {}
+      for index, row in kamusslang.iterrows():
+        if row[0] not in kata_pembakuan_dict:
+          kata_pembakuan_dict[row[0]] = row[1]
+      
+      dfprep['tokens_pembakuan'] = dfprep['tokens'].apply(pembakuan_kata)
+      dfprep['pembakuan'] = dfprep['tokens_pembakuan'].apply(lambda x:' '.join(x))
+
+      dfprep['pembakuan_bersih'] = dfprep['pembakuan'].apply(stop)
+      dfprep.dropna(inplace=True)
+      dfprep['temp_token'] = dfprep['pembakuan_bersih'].apply(lambda x: word_tokenize(str(x)))
+
+      st.markdown("**Hasil preprocessing data**")
+      st.dataframe(dfprep)
+
+      st.markdown("**Hasil analisis sentimen**")
+      text_vector = vectorizer.transform(dfprep['pembakuan_bersih'].apply(lambda x: str(x)))
+      dfprep['predicted_sentiment'] = svm_model.predict(text_vector)
+      dfresult = dfprep[['content', 'pembakuan_bersih','predicted_sentiment','temp_token']]
+      st.dataframe(dfresult[['content', 'pembakuan_bersih','predicted_sentiment']])
+
+      dfresultpos = dfresult[dfresult['predicted_sentiment'] == 1]
+      dfresultneg = dfresult[dfresult['predicted_sentiment'] == 0]
+
+      #count words
+      dffreq = hitung_kata(dfresult)
+      dffreqs = dffreq.reset_index()
+
+      #count words positive
+      dffreqpos = hitung_kata(dfresultpos)
+      dffreqposres = dffreqpos.reset_index()
+      dffreqpostop = dffreqposres.head(10)
+      
+      #count words negative
+      dffreqneg = hitung_kata(dfresultneg)
+      dffreqnegres = dffreqneg.reset_index()
+      dffreqnegtop = dffreqnegres.head(10)
+    
+
+      #plot count words positive
+      fig2pos = go.Figure()
+      fig2pos.add_trace(go.Bar(
+              x=dffreqpostop['freq'],
+              y=dffreqpostop['token'],
+              orientation='h',
+              marker=dict(color='#007AFF')
+            ))
+      fig2pos.update_layout(
+              title='Kata yang paling sering muncul pada ulasan positif',
+              xaxis_title='Frekuensi',
+              yaxis_title='Kata',
+              bargap=0.1
+            )
+      fig2pos.update_yaxes(autorange="reversed")
+      st.plotly_chart(fig2pos)
+
+      #plot count words negative
+      fig2neg = go.Figure()
+      fig2neg.add_trace(go.Bar(
+              x=dffreqnegtop['freq'],
+              y=dffreqnegtop['token'],
+              orientation='h',
+              marker=dict(color='#EE4B2B')
+            ))
+      fig2neg.update_layout(
+              title='Kata yang paling sering muncul pada ulasan negatif',
+              xaxis_title='Frekuensi',
+              yaxis_title='Kata',
+              bargap=0.1
+            )
+      fig2neg.update_yaxes(autorange="reversed")
+      st.plotly_chart(fig2neg)
+
+      #Wordcloud
+      reviews_text = ' '.join(dfprep['pembakuan_bersih'].tolist())
+      wordcloud = WordCloud(width=800, height=400, background_color='white').generate(reviews_text)
+
+      plt.figure(figsize=(10, 5))
+      plt.imshow(wordcloud, interpolation='bilinear')
+      plt.title("Wordcloud")
+      plt.axis('off')
+      st.pyplot(plt)
+
+      #Analisis Ngrams
+      dfprep['bigrams'] = dfprep['temp_token'].apply(ngrams,n=2)
+      df2 = hitung_kata(dfprep, 'bigrams').head()
+      df3 = df2.reset_index()
+
+      fig3 = px.bar(df3, x='token', y='freq', title='Analisis ngrams')
+      fig3.update_xaxes(tickangle=-90)
+      st.plotly_chart(fig3)
+
+      #Analisis Ngrams positif
+      dfresultpos['bigrams'] = dfresultpos['temp_token'].apply(ngrams, n=2)
+      dfngrampos = hitung_kata(dfresultpos, 'bigrams').head()
+      dfngrampos2 = dfngrampos.reset_index()
+
+      fig4 = px.bar(dfngrampos2, x='token', y='freq', title = 'Analisis Ngrams Sentimen Positif')
+      fig4.update_xaxes(tickangle =-90)
+      fig4.update_traces(marker_color = '#007AFF')
+      st.plotly_chart(fig4)
+
+      #Analisis Ngrams negatif
+      dfresultneg['bigrams'] = dfresultneg['temp_token'].apply(ngrams, n=2)
+      dfngramneg = hitung_kata(dfresultneg, 'bigrams').head()
+      dfngramneg2 = dfngramneg.reset_index()
+
+      fig5 = px.bar(dfngramneg2, x='token', y='freq', title = 'Analisis Ngrams Sentimen Negatif')
+      fig5.update_xaxes(tickangle=-90)
+      fig5.update_traces(marker_color = '#EE4B2B')
+      st.plotly_chart(fig5)
+
+      positive_proportion = dfresult['predicted_sentiment'].mean() * 100
+
+      st.subheader('Summary')
+      col1, col2, col3 = st.columns(3)
+      col1.metric("Rata-rata rating", round(df_filter.score.mean(),2))
+      col2.metric("Kata Teratas", dffreqs['token'][0])
+      col3.metric("Proporsi Sentimen Positif", f"{positive_proportion:.2f}%")
+
+      st.markdown('**Download Data**')
+      col4, col5 = st.columns(2)
+      with col4:
+        st.write("Download Data Hasil Scraping")
+        st.download_button(label = "Download", data=convertdf_tocsv(df_filter), file_name = 'df_filter.csv', mime = 'text/csv')
+
+      with col5:
+        st.write("Download Data Analisis Sentimen")
+        st.download_button(label = "Download", data=convertdf_tocsv(dfresult), file_name = 'dfresult.csv', mime = 'text/csv')
+      
+
+
+
+  elif choice == 'Tentang Kami':
+    st.title("Tentang Kami")
+    st.markdown("Terima kasih telah menggunakan aplikasi ini.")
+    st.markdown("Anda menggunakan aplikasi sentimen analisis versi beta I")
+
+
+if __name__ == '__main__':
+  main()
